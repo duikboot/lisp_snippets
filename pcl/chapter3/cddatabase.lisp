10d7a108467245530fa14089373ddbe21414682b
arjen.dijkstra <arjen.dijkstra@localhost>
2015-08-28 15:22:28 +0200

    Finalish

--- pcl/chapter3/cddatabase.lisp
+++ pcl/chapter3/cddatabase.lisp
@@ -39,22 +39,8 @@
   (with-open-file (in filename)
     (with-standard-io-syntax (setf *db* (read in)))))
 
-; (defun select-by-artist (artist)
-;   (remove-if-not #'(lambda (cd) (equal (getf cd :artist) artist)) *db*))
-
-; (defun artist-selector (artist)
-;     #'(lambda (cd) (equal (getf cd :artist) artist)))
-
 (defun select (selector-fn)
   (remove-if-not selector-fn *db*))
-
-; (defun where (&key title artist rating (ripped nil ripped-p))
-;   #'(lambda (cd)
-;       (and
-;         (if title    (equal (getf cd :title) title) t)
-;         (if artist   (equal (getf cd :artist) artist) t)
-;         (if rating   (equal (getf cd :rating) rating) t)
-;         (if ripped-p (equal (getf cd :ripped) ripped) t))))
 
 (defun update (selector-fn &key title artist rating (ripped nil ripped-p))
   (setf *db*
@@ -74,8 +60,24 @@
   `(equal (getf cd ,field) ,value))
 
 (defun make-comparison-list (fields)
-  (loop while fields collecting 
+  (loop while fields collecting
         (make-comparison-exp (pop fields) (pop fields))))
 
 (defmacro where (&rest clauses)
   `#'(lambda (cd) (and ,@(make-comparison-list clauses))))
+
+
+; (defun select-by-artist (artist)
+;   (remove-if-not #'(lambda (cd) (equal (getf cd :artist) artist)) *db*))
+
+; (defun artist-selector (artist)
+;     #'(lambda (cd) (equal (getf cd :artist) artist)))
+
+; (defun where (&key title artist rating (ripped nil ripped-p))
+;   #'(lambda (cd)
+;       (and
+;         (if title    (equal (getf cd :title) title) t)
+;         (if artist   (equal (getf cd :artist) artist) t)
+;         (if rating   (equal (getf cd :rating) rating) t)
+;         (if ripped-p (equal (getf cd :ripped) ripped) t))))
+
