2b61f4dcf24a9bcba6ac585e1c6742c8ca9a9200
arjen.dijkstra <arjen.dijkstra@localhost>
2015-09-07 16:24:12 +0200

    More refactor

--- pcl/chapter9/unit-test-framework.lisp
+++ pcl/chapter9/unit-test-framework.lisp
@@ -1,12 +1,14 @@
 (defun report-result (result form)
   (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
-(defmacro check (form)
-  `(report-result ,form ',form))
+(defmacro check (&body form)
+  `(progn
+     ,@(loop for f in form collect `(report-result ,f ',f))))
 
 
 (defun tests ()
-  (check (= (+ 1 2) 3))
-  (check (= (+ 1 2 3) 6))
-  (check (= (+ 1 -2) -1))
-  (check (= (- 1 2) 0)))
+  (check
+    (= (+ 1 2 3) 6)
+    (= (+ 1 -2) -1)
+    (= (+ 2 -2) -1)
+    (= (+ 1 2) 3)))
