4e728e73763e092d94da11366a4724777dba1f53
arjen.dijkstra <arjen.dijkstra@localhost>
2015-09-07 15:22:42 +0200

    macro

--- pcl/chapter9/unit-test-framework.lisp
+++ pcl/chapter9/unit-test-framework.lisp
@@ -2,5 +2,7 @@
   (format t "~:[FAIL~;pass~] ... ~a~%" result form))
 
 (defun test-+ ()
-  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3))
-  )
+  (report-result (= (+ 1 2) 3) '(= (+ 1 2) 3)))
+
+(defmacro check (form)
+  `(report-result ,form ',form))
