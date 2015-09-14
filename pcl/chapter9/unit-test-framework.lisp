2eeaef794077befee6280f0aef5fc2e175e7ff31
arjen.dijkstra <arjen.dijkstra@localhost>
2015-09-14 12:06:35 +0200

    extra test

--- pcl/chapter9/unit-test-framework.lisp
+++ pcl/chapter9/unit-test-framework.lisp
@@ -6,10 +6,10 @@
   `(progn
      ,@(loop for f in form collect `(report-result ,f ',f))))
 
-
 (defun tests ()
   (check
     (= (+ 1 2 3) 6)
     (= (+ 1 -2) -1)
     (= (+ 2 -2) -1)
+    (= (+ 3 -2) -1)
     (= (+ 1 2) 3)))
