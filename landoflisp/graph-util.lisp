698cc4fa1521425d40d5d79670cf6e334005d1c0
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2015-10-25 21:37:59 +0100

    new function

--- landoflisp/graph-util.lisp
+++ landoflisp/graph-util.lisp
@@ -7,3 +7,10 @@
 (defparameter *max-label-length* 30)
 
 
+(defun dot-label (exp)
+  (if exp
+    (let ((s (write-to-string exp :pretty nil)))
+      (if (> (length s) *max-label-length*)
+        (concatenate 'string (subseq s 0 (- *max-label-length* 3)) "...")
+        s))
+    ""))
