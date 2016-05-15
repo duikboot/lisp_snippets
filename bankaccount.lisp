0f12bf1df0dc053cfd6a125f579c4024927342ae
Arjen Dijkstra <dijkstra.arjen@gmail.com>
2016-05-15 15:02:10 +0200

    closure example

---
+++ bankaccount.lisp
@@ -0,0 +1,17 @@
+(defgeneric withdraw (account amount)
+  (:documentation "Withdraw account with amount,
+                   Signal error if current balance is less than amount"))
+
+(defmethod withdraw ((account bank-account) amount)
+  (when (< (balance account) amount)
+    (error "Account overdrawn."))
+  (decf (balance account) amount))
+
+
+(defmethod withdraw ((account checking-account) amount)
+  (let ((overdraft (- amount (balance account))))
+    (when (plusp overdraft)
+      (withdraw (overdraft-account account) overdraft)
+      (incf (balance account) overdraft)))
+  (call-next-method))
+
